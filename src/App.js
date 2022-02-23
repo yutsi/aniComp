1784a4df23f68df65689de58ed69f0f5e00f4671
yutsi <darthyutsi1@gmail.com>
2022-02-23 15:37:14 -0500

    initialized Bootstrap with Cyborg theme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import Error from './components/Error'
 import Media from './components/Media'
 import { combineLists } from './services/ops'
+import { Container, Row, Col } from 'react-bootstrap'
+import './custom.scss'
 
 const App = () => {
   const [user1, setuser1] = useState('')
@@ -69,62 +71,58 @@ const App = () => {
   }
 
   return (
-    <div className='main'>
-      <div className='content-wrap'>
-        <div className='entry-area'>
-          <h1>Compare media lists of two AniList users</h1>
-          <div className='description'>Scores are converted to a 10-point scale.</div>
-          <form className='add-table' onSubmit={compareUsers}>
-            <div className='row'>
-              <label htmlFor='enter user1' className='new-entry-label'>
-                user 1:
-              </label>
-              <input type='text' id='enter user1' aria-label='enter user1' className='new-entry' onChange={handleUser1Change} />
-            </div>
-            <div className='row'>
-              <label htmlFor='enter user2' className='new-entry-label'>
-                user 2:
-              </label>
-              <input type='text' id='enter user2' aria-label='enter user2' className='new-entry' onChange={handleUser2Change} />
-            </div>
-            <div className='row'>
-              <input className='radio' type='radio' id='anime' name='mediaType' value='ANIME' checked={checked} onChange={handleCheckedChange} />
-              <label htmlFor='anime'>anime</label>
-              <input className='radio' type='radio' id='manga' name='mediaType' value='MANGA' checked={!checked} onChange={handleCheckedChange} />
-              <label htmlFor='manga'>manga</label>
-            </div>
-            <label htmlFor='submit'>
-              <button type='submit' id='submit' aria-label='submit'>Compare</button>
+    <Container fluid>
+      <Row>
+        <h1>Compare media lists of two AniList users</h1>
+        <div className='description'>Scores are converted to a 10-point scale.</div>
+        <form className='add-table' onSubmit={compareUsers}>
+          <div className='row'>
+            <label htmlFor='enter user1' className='new-entry-label'>
+              user 1:
             </label>
-          </form>
-          <Error message={message} />
-        </div>
+            <input type='text' id='enter user1' aria-label='enter user1' className='new-entry' onChange={handleUser1Change} />
+          </div>
+          <div className='row'>
+            <label htmlFor='enter user2' className='new-entry-label'>
+              user 2:
+            </label>
+            <input type='text' id='enter user2' aria-label='enter user2' className='new-entry' onChange={handleUser2Change} />
+          </div>
+          <div className='row'>
+            <input className='radio' type='radio' id='anime' name='mediaType' value='ANIME' checked={checked} onChange={handleCheckedChange} />
+            <label htmlFor='anime'>anime</label>
+            <input className='radio' type='radio' id='manga' name='mediaType' value='MANGA' checked={!checked} onChange={handleCheckedChange} />
+            <label htmlFor='manga'>manga</label>
+          </div>
+          <label htmlFor='submit'>
+            <button type='submit' id='submit' aria-label='submit'>Compare</button>
+          </label>
+        </form>
+        <Error message={message} />
+      </Row>
 
-        <div className='compare-area'>
-          <h2>{user1compared || 'user1'} vs. {user2compared || 'user2'}</h2>
-          <h3 className='agree'>Agree</h3>
-          <table className='media' aria-label='agree-media'>
-            <tbody>
-              {combined.filter((media) => media.scoreDifference <= 1)
-                .map((media) => <Media key={media.mediaId} media={media} user1={user1compared} user2={user2compared} />)}
-            </tbody>
-          </table>
+      <Row>
+        <h2>{user1compared || 'user1'} vs. {user2compared || 'user2'}</h2>
+        <h3 className='agree'>Agree</h3>
+        <table className='media' aria-label='agree-media'>
+          <tbody>
+            {combined.filter((media) => media.scoreDifference <= 1)
+              .map((media) => <Media key={media.mediaId} media={media} user1={user1compared} user2={user2compared} />)}
+          </tbody>
+        </table>
 
-          <h3 className='disagree'>Disagree</h3>
-          <table className='media' aria-label='disagree-media'>
-            <tbody>
-              {combined.filter((media) => media.scoreDifference > 1)
-                .map((media) => <Media key={media.mediaId} media={media} user1={user1compared} user2={user2compared} />)}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <footer>
-        <div className='footer'>
-          This website was made by yutsi using React and the AniList API. Check it out on <a href='https://github.com/yutsi/aniComp'>Github</a>.
-        </div>
-      </footer>
-    </div>
+        <h3 className='disagree'>Disagree</h3>
+        <table className='media' aria-label='disagree-media'>
+          <tbody>
+            {combined.filter((media) => media.scoreDifference > 1)
+              .map((media) => <Media key={media.mediaId} media={media} user1={user1compared} user2={user2compared} />)}
+          </tbody>
+        </table>
+      </Row>
+      <Row>
+        This website was made by yutsi using React and the AniList API. Check it out on <a href='https://github.com/yutsi/aniComp'>Github</a>.
+      </Row>
+    </Container>
   )
 }
 
